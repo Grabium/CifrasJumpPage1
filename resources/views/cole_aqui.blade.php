cc9fc30343d504c1e9656345379b006d7d2b9ce4
Grabium <110872805+Grabium@users.noreply.github.com>
2023-11-24 02:47:43 +0000

    #3 página pronta para mobile. Falta re-escolher a paleta de cores.

--- resources/views/cole_aqui.blade.php
+++ resources/views/cole_aqui.blade.php
@@ -1,7 +1,7 @@
 @extends('top')
 @section('content')
 <div id="div_page">
-<div id="div_coleabaixo"><h2>Cole o texto abaixo</h2></div>
+
 
 
 <?php 
@@ -10,18 +10,27 @@
 
   <form action="{{ route('enviaTexto') }}" method='POST'>
   @csrf
-    <hr />
+
+
     <div id="div_alterar"><div id="div_filha_alterar">
       <input name="semiTons" id="semiTons" value={{ $semiTons }} />
       <label id="lab_semitons">0</label>
-      <button id="but_alterar"class="btn btn-danger" '>Alterar</button>
+      <button id="but_alterar" class="btn btn-danger" >Alterar</button>
     </div></div>
-    <hr />
-    <button id="diminuir" class="btn btn-danger" onclick="intervalo('-')">-</button>
-    <button id="aumentar" class="btn btn-danger" onclick='intervalo("+")'>+</button>
-    <!--button class="btn btn-danger" >Alterar</button-->
-    <br/><br/>
-    <textarea name="aSeparar" rows="40" cols="120" class="container-fluid -body-color"></textarea>
+
+    <div class="div_coleabaixo">Ajuste a quantidade em SEMI-TONS:</div>
+
+    <div id="div_operacoes"><div id="div_filha_operacoes">
+      <button id="diminuir" class="btn btn-danger" onclick="intervalo('-')">-</button>
+      <button id="aumentar" class="btn btn-danger" onclick='intervalo("+")'>+</button>
+    </div></div>
+
+    <div class="div_coleabaixo">Insira o texto no campo abaixo:</div>
+    
+
+    <div id="div_text">
+      <textarea id="aSeparar" name="aSeparar" rows="10" cols="120" class="container-fluid -body-color"></textarea>
+    </div>
   </form>
 </div><!-- page -->
 
